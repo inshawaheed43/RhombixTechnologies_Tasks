51a85d42ef2447c6770557b4d171f63cc724d3da
Insha Waheed <waheedinsha02@gmail.com>
2025-11-08 00:00:38 +0500

    All Fixed

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
 plugins: [react(), tailwindcss()],
-base:'/RhombixTechnologies_Tasks/'
+base:"/RhombixTechnologies_Tasks/"
 })
